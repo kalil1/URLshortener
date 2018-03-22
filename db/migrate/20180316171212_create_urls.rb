4aea2f29e94ab4b43a2c538f40bf735eb947690c
kalil williams <wkalil416@gmail.com>
2018-03-22 14:53:48 -0400

    fixed

--- db/migrate/20180316171212_create_urls.rb
+++
@@ -1,8 +0,0 @@
-class CreateUrls < ActiveRecord::Migration[5.1]
-  def change
-    create_table :urls do |t|
-
-      t.timestamps
-    end
-  end
-end
--- test/system/urls_test.rb
+++
@@ -1,9 +0,0 @@
-require "application_system_test_case"
-
-class UrlsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit urls_url
-  #
-  #   assert_selector "h1", text: "Url"
-  # end
-end
